baaca41aad78e4549298edf31b31fd1eeb127d05
전현찬 <2020014975@hanyang.ac.kr>
2026-04-17 22:55:08 +0900

    2DGameBaisc02 Done

---
+++ 2DGameBasic02/Assets/CollisionEvent.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class CollisionEvent : MonoBehaviour
+{
+    [SerializeField]
+    private Color          color;
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        spriteRenderer.color = color;
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        Debug.Log(gameObject.name + " : OnCollisionStay2D() 메소드 실행");
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        spriteRenderer.color = Color.white;
+    }
+}
